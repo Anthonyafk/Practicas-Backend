922b889e4e06ec13750690e17f4e7f36beecf4ec
Francisco Daniel García López <Francisco_Daniel_Garcia_Lopez@ciencias.unam.mx>
2025-10-31 20:36:40 -0600

    feat: implement Product and Product-image

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -1,5 +1,6 @@
 package com.product.api.controller;
 
+import com.product.api.dto.in.DtoCategoryIn;
 import com.product.api.entity.Category;
 import com.product.api.service.SvcCategory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,31 +8,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory;
+import com.product.commons.dto.ApiResponse;
 import java.util.List;
 import java.util.Optional;
+import jakarta.validation.Valid;
 
 /**
  * Controlador REST para manejar las operaciones CRUD de categorías.
  */
 @RestController
-@RequestMapping("/Category")
-@CrossOrigin(origins = "*")
+@RequestMapping("/category")
 public class CtrlCategory {
+	
+	@Autowired
+	SvcCategory svc;
 
-    private final SvcCategory svcCategory;
-
-    @Autowired
-    public CtrlCategory(SvcCategory svcCategory) {
-        this.svcCategory = svcCategory;
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		return ResponseEntity.ok(svc.findAll());
     }
 
-    /**
-     * Obtiene todas las categorías activas de la base de datos.
-     * 
-     * @return ResponseEntity con la lista de categorías activas
-     */
-    @GetMapping("")
-    public ResponseEntity<List<Category>> findAll() {
-        return svcCategory.findAll();
-    }
-}
+    	@GetMapping("/active")
+	public ResponseEntity<List<Category>> findActive(){
+		return ResponseEntity.ok(svc.findActive());
+	}
+
+    	@PostMapping
+	public ResponseEntity<ApiResponse> create(@Valid @RequestBody DtoCategoryIn in) {
+		return ResponseEntity.ok(svc.create(in));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ApiResponse> update(@Valid @RequestBody DtoCategoryIn in, @PathVariable("id") Integer id){
+		return ResponseEntity.ok(svc.update(in, id));
+	}
+
+	@PatchMapping("/{id}/enable")
+	public ResponseEntity<ApiResponse> enable(@PathVariable Integer id) {
+		return ResponseEntity.ok(svc.enable(id));
+	}
+
+	@PatchMapping("/{id}/disable")
+	public ResponseEntity<ApiResponse> disable(@PathVariable Integer id) {
+		return ResponseEntity.ok(svc.disable(id));
+	}
+} 
--- product/src/main/java/com/product/api/controller/CtrlProduct.java
+++ product/src/main/java/com/product/api/controller/CtrlProduct.java
@@ -1,28 +1,63 @@
 package com.product.api.controller;
-import com.product.api.entity.Category;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.product.api.dto.in.DtoProductIn;
+import com.product.api.dto.out.DtoProductListOut;
+import com.product.api.dto.out.DtoProductOut;
+import com.product.api.service.SvcProduct;
+import com.product.commons.dto.ApiResponse;
+import com.product.exception.ApiException;
 
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/product")
 public class CtrlProduct {
 
-    @GetMapping("/Lista")
-    public List<Category> getCategories() {
-        return List.of(
-            new Category(1, "Lentes", "Lts", 1),
-            new Category(2, "Relojes", "Rljs", 1),
-            new Category(3, "Billeteras", "Bllts", 1),
-            new Category(4, "Mochilas", "Mchls", 1),
-            new Category(5, "Peluches", "Pelch", 1),
-            new Category(6, "Papas", "Paps", 1)
-        );
-    }
+	@Autowired
+	SvcProduct svc;
+
+	@GetMapping
+	public ResponseEntity<List<DtoProductListOut>> getProducts() {
+		return svc.getProducts();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<DtoProductOut> getProduct(@PathVariable Integer id) {
+		return svc.getProduct(id);
+	}
+
+	@PostMapping
+	public ResponseEntity<ApiResponse> createProduct(@Valid @RequestBody DtoProductIn in) {
+		return svc.createProduct(in);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ApiResponse> updateProduct(@PathVariable Integer id, @Valid @RequestBody DtoProductIn in) {
+		return svc.updateProduct(id, in);
+	}
+
+	@PatchMapping("/{id}/enable")
+	public ResponseEntity<ApiResponse> enableProduct(@PathVariable Integer id) {
+		return svc.enableProduct(id);
+	}
+
+	@PatchMapping("/{id}/disable")
+	public ResponseEntity<ApiResponse> disableProduct(@PathVariable Integer id) {
+		return svc.disableProduct(id);
+	}
 }
